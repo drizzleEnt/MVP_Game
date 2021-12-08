860ffecb7bf1f023bdd494b22a6db2415957e84b
drizzleEnt <70242740+drizzleEnt@users.noreply.github.com>
2021-12-08 21:14:43 +0300

    Update PlayerModel.cs

--- Assets/Sourse/Model/PlayerModel.cs
+++ Assets/Sourse/Model/PlayerModel.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 public class PlayerModel : IUpdatable
 {
     [SerializeField] private float _force = 300f;
-    [SerializeField] private Vector2 _offset = Vector2.right;
+    [SerializeField] private float _speed = 3f;
 
+    private Vector2 _offset = Vector2.right;
     private float _movingTime = 0;
+    private Vector2 _moveDirection;
+    private Vector2 _newPosition;
     private Rigidbody2D _rigidbody;
+    private Transform _playerTransform;
 
     public void Init(Rigidbody2D rigidbody)
     {
         _rigidbody = rigidbody;
+        _playerTransform = _rigidbody.GetComponent<Transform>();
     }
     
     public void Update(float deltaTime)
     {
         if (_movingTime <= 0)
         {
-
             return;
         }
 
         _movingTime -= deltaTime;
+        _playerTransform.Translate(_moveDirection * _speed * deltaTime);
     }
 
     public void MoveSides(float direction)
     {
         Vector2 currentPosition = _rigidbody.position;
-        Vector2 newPosition = currentPosition + _offset * direction;
-        Vector2 moveDirection = newPosition - currentPosition;
-        _rigidbody.AddForce(moveDirection, ForceMode2D.Impulse);
+        _newPosition = currentPosition + _offset * direction;
+        _moveDirection = _newPosition - currentPosition;
+        //_rigidbody.AddForce(moveDirection, ForceMode2D.Impulse);
+        
+
     }
 
     public void SetMovingAbillity(float movingTime)
